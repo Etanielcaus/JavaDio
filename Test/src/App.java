8b8010cb583e5e6524b1df4e9f040bf8ed82d454
Etaniel Caus <nielfilosofia@gmail.com>
2024-03-23 00:11:34 -0300

    test

---
+++ Test/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        
+        Testingclass testingclass = new Testingclass("Etaniel", 22);
+        System.out.println(testingclass.getName());
+    }
+}
